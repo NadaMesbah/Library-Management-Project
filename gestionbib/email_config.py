ddb506ccbc6ddd6fa9544b1f48a69404be8a38d6
nada <nada.mesbah@usmba.ac.ma>
2024-07-13 11:50:21 +0100

    Add .gitignore to ignore email configuration file

---
+++ gestionbib/email_config.py
@@ -0,0 +1,6 @@
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'nada.mesbah@usmba.ac.ma'
+EMAIL_HOST_PASSWORD = 'raykod2003'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
